5b6dc62ff7fbaad142e133634fd6267527425bd2
Anahita <shakofa.mahmody@gmail.com>
2021-04-09 23:07:30 -0700

    add the exercise and stats routes to view.js file

---
+++ routes/view.js
@@ -0,0 +1,15 @@
+const router = require('express');
+const path = require('path');
+
+
+//exercise route
+router.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+});
+
+//stats routes
+router.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
+});
+
+module.exports = router;

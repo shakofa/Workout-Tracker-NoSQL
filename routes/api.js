6129b8e6fb44b05360ecbd780f76707585799b3a
Anahita <shakofa.mahmody@gmail.com>
2021-04-16 00:04:22 -0700

    spelling correction

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const workout = require('../models/workout.js');
+const Workout = require('../models/workout.js');
 
 
 //this route create the workout database
